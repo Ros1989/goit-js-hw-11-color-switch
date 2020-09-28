77ca50c0c4afcfcd6ba4eb856c7edc131d67300f
Rostyslav Zhabinskyi <zhabinskiirostislav@gmail.com>
2020-09-28 18:48:55 +0200

    changed some files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-'use strict';
-import './css/styles.css';
-import './js/colorSwitch.js';
+import './styles.css';
+import { colors } from './js/colors';
+import { refs } from './js/refs';
+let intervalID = null;
+
+refs.startBtn.addEventListener('click', handleStartBtn);
+refs.stopBtn.addEventListener('click', handleStopBtn);
+
+function handleStartBtn() {
+  intervalID = setInterval(() => {
+    const randomNum = randomIntegerFromInterval(0, colors.length - 1);
+    const text = randomIntegerFromInterval(0, colors.length - 1);
+    refs.body.style.backgroundColor = colors[randomNum];
+    refs.h1.style.color = colors[text];
+  }, 1000);
+
+  refs.startBtn.setAttribute('disabled', 'disabled');
+}
+
+function handleStopBtn() {
+  clearInterval(intervalID);
+
+  refs.startBtn.removeAttribute('disabled');
+
+  refs.body.style.backgroundColor = '#ffffff';
+  refs.h1.style.color = '#000000';
+}
+
+function randomIntegerFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+export const refs = {
+  startBtn: document.querySelector('button[data-action="start"]'),
+  stopBtn: document.querySelector('button[data-action="stop"]'),
+  body: document.querySelector('body'),
+  h1: document.querySelector('h1'),
+};
